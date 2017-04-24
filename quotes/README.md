578e290ffff67bb8dc8f4f15a277f432fad878b4
Allister <allisterkmoon@gmail.com>
2017-04-23 17:41:07 -0700

    Make the Quote Machine better
    Gave it a Dave Barry theme, because that guy is awesome. Redid stuff,
    cleaned up code.

---
+++ quotes/README.md
@@ -0,0 +1,14 @@
+# Personal Portfolio
+
+Link to live page:
+http://www.allistermoon.com/freecodecamp/quotes
+
+Link to the FCC challenge page:
+https://www.freecodecamp.com/challenges/build-a-random-quote-machine
+
+__Objective:__
+Build an app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/ONjoLe/
+
+__User Stories:__
+- I can click a button to show me a new random quote.
+- I can press a button to tweet out a quote.
--- quotes/index.js
+++ quotes/index.js
@@ -10,17 +10,35 @@ $(document).ready(function() {
 
 function generateQuote() {
   var quotes = [
-    "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.&Francis of Assisi",
-    "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.&Jimmy Dean",
-    "Perfection is not attainable, but if we chase perfection we can catch excellence.&Vince Lombardi",
-    "If opportunity doesn't knock, build a door.&Milton Berle",
-    "Don’t worry about people stealing your design work. Worry about the day they stop.&Jeffrey Zeldman",
-    "Always look for the fool in the deal. If you don’t find one, it’s you.&Mark Cuban"
+    "Electricity is actually made up of extremely tiny particles called electrons, that you cannot see with the naked eye unless you have been drinking.",
+    "The only really good place to buy lumber is at a store where the lumber has already been cut and attached together in the form of furniture, finished, and put inside boxes.",
+    "Without question, the greatest invention in the history of mankind is beer. Oh, I grant you that the wheel was also a fine invention, but the wheel does not go nearly as well with pizza.",
+    "Denmark (also called \"Norway\") is best known as the original home of the prune Danish as well as the Vikings, who wore hats with horns sticking out of them, and for a very good reason: they were insane.",
+    "The best way to learn Japanese is to be born as a Japanese baby, in Japan, raised by a Japanese family.",
+    "We need our highest judicial body to stop this childish bickering and get back to debating the kinds of weighty constitutional issues that have absorbed the court in recent years, such as whether a city can legally force an exotic dancer to cover her entire nipple, or just the part that pokes out.",
+    "What was life like in the colonies? Probably the best word to describe it would be \"colonial\".",
+    "It is a scientific fact that your body will not absorb cholesterol if you take it from another person's plate.",
+    "Winter's here, and you feel lousy: You're coughing and sneezing; your muscles ache; your nose is an active mucus volcano. These symptoms -- so familiar at this time of year -- can mean only one thing: Tiny fanged snails are eating your brain.",
+    "We must always remember that, as Americans, we all have a common enemy -- an enemy that is dangerous, powerful and relentless. I refer, of course, to the federal government.",
+    "The real threat to whales is whaling, which has endangered many whale species.",
+    "The one thing that unites all human beings, regardless of age, gender, religion or ethnic background, is that we all believe we are above-average drivers.",
+    "It is a well-documented fact that guys will not ask for directions. This is a biological thing. This is why it takes several million sperm cells... to locate a female egg, despite the fact that the egg is, relative to them, the size of Wisconsin.",
+    "Camping is nature's way of promoting the motel business.",
+    "Nobody cares if you can't dance well. Just get up and dance.",
+    "People who want to share their religious views with you almost never want you to share yours with them.",
+    "The word user is the word used by the computer professional when they mean idiot.",
+    "Fishing is boring, unless you catch an actual fish, and then it is disgusting.",
+    "You can only be young once. But you can always be immature.",
+    "Gravity is a contributing factor in nearly 73 percent of all accidents involving falling objects.",
+    "Never under any circumstances take a sleeping pill and a laxative on the same night.",
+    "Never assume that the guy understands that you and he have a relationship.",
+    "The major parties could conduct live human sacrifices on their podiums during prime time, and I doubt that anybody would notice.",
+    "To better understand why you need a personal computer, let's take a look at the pathetic mess you call your life."
   ];
 
   var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
   randomQuote = randomQuote.split('&');
   $('.quote-text').text(randomQuote[0]);
-  $('.quote-author').text(randomQuote[1]);
-  $('#tweet-button').attr('href', "http://twitter.com/home?status=" + randomQuote[0]);
+  $('.quote-author').text('Dave Barry');
+  $('#tweet-button').attr('href', 'https://twitter.com/intent/tweet?text=' + randomQuote[0]);
 }
