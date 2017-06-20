e114a10ee7ea469a488da8ee3e89fdffb66cb77e
Allister <allisterkmoon@gmail.com>
2017-06-19 23:22:14 -0700

    Change links to absolute in api project
    Couldn't figure out how to get the rewrite rules working on my NGINX
    server without breaking relative links, so switching to absolute links
    instead.

--- api-timestamp/public/submit.js
+++ api-timestamp/public/submit.js
@@ -1,11 +1,13 @@
-function submit () {
-  var datetime = document.getElementById('datetime').value
-  window.location.href = '/' + datetime
+function submit (datetime) {
+  if (value === undefined) {
+    datetime = document.getElementById('datetime').value
+  }
+  window.location.href = 'http://freecodecamp.allistermoon.com/api-timestamp/' + datetime
 }
 
 function submitRandom () {
   var range = 100000000000
 
   var datetime = Math.floor(Math.random() * 2 * range) - range
-  window.location.href = '/' + datetime
+  window.location.href = 'http://freecodecamp.allistermoon.com/api-timestamp/' + datetime
 }
