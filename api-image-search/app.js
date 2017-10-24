2c235c95173a1279c434e58fd29068f99b2d4729
Allister <allisterkmoon@gmail.com>
2017-10-23 19:30:55 -0700

    Added support for search properties
    Added in parsing of additional properties to the API, set up just a few
    for now (offset, count, exactTerms, excludeTerms) but set up ability to
    add any number without any rewrites.

--- api-image-search/app.js
+++ api-image-search/app.js
@@ -8,6 +8,14 @@ const request = require('request')
 
 require('dotenv').config()
 
+// key = search property in my API, val = search property in Google API
+const extraAllowedProps = {
+  offset:       'start',
+  count:        'num',
+  exactTerms:   'exactTerms',
+  excludeTerms: 'excludeTerms'
+}
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
@@ -15,7 +23,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/search/*', function (req, res) {
-  getGoogle(req.params[0]).then(data => {
+  getGoogle(req.params[0], req.query).then(data => {
     res.setHeader('content-type', 'application/json')
     res.send(data)
   }, err => {
@@ -28,13 +36,37 @@ app.listen(port, function () {
   console.log(`FCC - Image Search Abstraction Layer: Listening on port ${port}`)
 })
 
-function getGoogle(query) {
+function getGoogle(query, props) {
   return new Promise((resolve, reject) => {
     let url = `${process.env.SEARCH_URL}?key=${process.env.API_KEY}&cx=${process.env.SEARCH_ID}&q=${query}`
 
-    request.get(url, (err, res, body) => {
+    let properties = {
+      key:  process.env.API_KEY,
+      cx:   process.env.SEARCH_ID,
+      q:    query
+    }
+    // Add any additional properties to the search query, while taking into account any possible naming differences
+    // that I have for some reason decided to do (e.g. 'start' in the Google API to 'offset' in mine )
+    for (let myProp in extraAllowedProps) {
+      if (extraAllowedProps.hasOwnProperty(myProp)) {
+        if (props[myProp]) properties[extraAllowedProps[myProp]] = props[myProp]
+      }
+    }
+
+    request({url: url, qs: properties}, (err, res, body) => {
       if (err) reject(err)
-      resolve(body)
+      resolve(formatApiData(body))
     })
   })
+}
+
+function formatApiData(data) {
+  let result = {}
+  try {
+    result.results = JSON.parse(data).items
+  } catch(err) {
+    return { error: err }
+  }
+
+  return result
 }
