180178e23c08a80893355f41dbd832098ae19453
Allister <allisterkmoon@gmail.com>
2018-03-18 16:22:16 -0700

    Allow uploading and reading files via api

--- api-file-metadata/app.js
+++ api-file-metadata/app.js
@@ -1,5 +1,9 @@
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: '/tmp' })
+
 const app = express()
 const port = 50024
 
@@ -9,6 +13,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.send(pug.renderFile(path.join(__dirname, '/index.pug')))
+})
+app.post('/api', upload.array('files', 10), (req, res, next) => {
+  res.json(req.files)
+  req.files.forEach(file => {
+    fs.unlink(file.path, err => {
+      if (err) console.log(`Error when trying to delete file: ${err}`)
+    })
+  })
+
 })
 
 app.listen(port, () => {
