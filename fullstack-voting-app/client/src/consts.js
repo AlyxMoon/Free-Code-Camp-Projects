adb609d8b8c592c075879ed8563359207efcaaaf
Allister <allisterkmoon@gmail.com>
2018-04-14 08:39:33 -0700

    Update server paths again

--- fullstack-voting-app/client/src/consts.js
+++ fullstack-voting-app/client/src/consts.js
@@ -1,2 +1,2 @@
-export const serverPath = 'http://freecodecamp.allistermoon.com/fullstack-voting-app'
-export const apiPath = 'http://freecodecamp.allistermoon.com/fullstack-voting-app/api'
+export const serverPath = 'http://fullstack-voting-app.allistermoon.com/'
+export const apiPath = 'http://fullstack-voting-app.allistermoon.com/api'
