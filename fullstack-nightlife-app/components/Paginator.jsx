c5560e924dbf1794bf157ca4f6f4a73d88792a60
Allister <allisterkmoon@gmail.com>
2018-05-09 16:45:00 -0700

    Add 'jump to' feature for pagination

--- fullstack-nightlife-app/components/Paginator.jsx
+++ fullstack-nightlife-app/components/Paginator.jsx
@@ -1,7 +1,9 @@
+import { Component } from 'react'
 import PropTypes from 'prop-types'
 import { DEFAULT_API_QUERY_LIMIT } from '../lib/consts'
 
 import Link from 'next/link'
+import Router from 'next/router'
 
 const PaginatorLink = props => (
   <Link as="/" href={{ pathname: '/', query: { offset: props.number, location: props.location } }}>
@@ -14,6 +16,7 @@ const PaginatorLink = props => (
           border: 1px solid #DDD;
           border-radius: 4px;
           display: inline-block;
+          font-weight: bold;
           margin: 1px;
           padding: 5px;
           height: 20px;
@@ -36,87 +39,129 @@ PaginatorLink.propTypes = {
   text: PropTypes.string
 }
 
-const Paginator = props => {
-  let maxLinks = 5
-  let limit = Math.ceil(props.total / props.countPerPage)
+class Paginator extends Component {
+  static getDerivedStateFromProps (nextProps, prevState) {
+    let maxLinks = 5
+    let limit = Math.ceil(nextProps.total / prevState.countPerPage)
 
-  let start = props.currentPage - 3
-  if (start + maxLinks > limit) {
-    start = limit - maxLinks
+    let start = nextProps.currentPage - 3
+    if (start + maxLinks > limit) {
+      start = limit - maxLinks
+    }
+    if (start < 0) start = 0
+
+    let pageNumbers = []
+    for (let i = start, count = 0; i < limit && count < maxLinks; i++, count++) {
+      pageNumbers.push(i)
+    }
+
+    return { limit, pageNumbers }
   }
-  if (start < 0) start = 0
 
-  let pageNumbers = []
-  for (let i = start, count = 0; i < limit && count < maxLinks; i++, count++) {
-    pageNumbers.push(i)
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      gotoPage: '',
+      limit: 0,
+      pageNumbers: [],
+      countPerPage: DEFAULT_API_QUERY_LIMIT
+    }
+
+    this.handleChangeGotoPage = this.handleChangeGotoPage.bind(this)
+    this.goto = this.goto.bind(this)
   }
 
-  return (
-    <div className="paginator">
-      <ul className="pagination">
-        <PaginatorLink
-          number={0}
-          location={props.location}
-          text="<<"
-        />
-        <PaginatorLink
-          number={props.currentPage - 2}
-          location={props.location}
-          text="<"
-        />
-        {pageNumbers.map((i) => (
+  handleChangeGotoPage (event) {
+    this.setState({ gotoPage: event.target.value })
+  }
+
+  goto () {
+    let page = this.state.gotoPage
+    if (page > this.state.limit) page = this.state.limit
+    if (page < 1) page = 1
+
+    Router.push({
+      pathname: '/',
+      query: {
+        location: this.props.location,
+        offset: page - 1
+      }
+    }, '/')
+  }
+
+  render () {
+    let props = this.props
+
+    return (
+      <div className="paginator">
+        <ul className="pagination-item">
           <PaginatorLink
-            key={`pagination-${i}`}
-            number={i}
+            number={0}
             location={props.location}
-            active={(i + 1) === props.currentPage}
+            text="<<"
           />
-        ))}
-        <PaginatorLink
-          number={props.currentPage}
-          location={props.location}
-          text=">"
-        />
-        <PaginatorLink
-          number={limit - 1}
-          location={props.location}
-          text=">>"
-        />
-      </ul>
-      <hr />
-      <style jsx>{`
-        .paginator {
-          max-height: 70px;
-          overflow: hidden;
-        }
+          <PaginatorLink
+            number={props.currentPage - 2}
+            location={props.location}
+            text="<"
+          />
+          {this.state.pageNumbers.map((i) => (
+            <PaginatorLink
+              key={`pagination-${i}`}
+              number={i}
+              location={props.location}
+              active={(i + 1) === props.currentPage}
+            />
+          ))}
+          <PaginatorLink
+            number={props.currentPage}
+            location={props.location}
+            text=">"
+          />
+          <PaginatorLink
+            number={this.state.limit - 1}
+            location={props.location}
+            text=">>"
+          />
+        </ul>
+        <p className="pagination-item">
+          <label className="pagination-label" htmlFor="goto">Goto Page:</label>
+          <input
+            name="goto"
+            type="number"
+            min="0"
+            max={this.state.limit}
+            value={this.state.gotoPage}
+            onChange={this.handleChangeGotoPage} />
+          <input type="button" value="go" onClick={this.goto} />
+        </p>
+        <hr />
+        <style jsx>{`
+          .pagination-item {
+            display: flex;
+            justify-content: center;
+            padding-left: 0;
+          }
 
-        .pagination {
-          display: flex;
-          justify-content: center;
-          padding-left: 0;
-        }
+          .pagination-label {
+            font-weight: bold;
+            margin-right: 5px;
+          }
 
-        .pagination-label {
-          font-weight: bold;
-        }
-
-        ul {
-          list-style: none;
-          display: inline-block;
-        }
-      `}</style>
-    </div>
-  )
-}
-
-Paginator.defaultProps = {
-  countPerPage: DEFAULT_API_QUERY_LIMIT
+          ul {
+            list-style: none;
+            display: inline-block;
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
 
 Paginator.propTypes = {
   location: PropTypes.string.isRequired,
   total: PropTypes.number.isRequired,
-  countPerPage: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired
 }
 
